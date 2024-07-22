0ba2d6ea5c4d0ca913bdcd5280d7760f426ab428
= <=>
2024-07-22 16:32:51 +0800

    wow i actually made it (i stole off the download button lol)

--- script.js
+++ script.js
@@ -10,16 +10,16 @@ var drawingBoard = {
     },
     clear: function() {
         ctx.clearRect(0, 0, this.width, this.height);
+        ctx.fillStyle = `rgba(${bgr}, ${bgg}, ${bgb}, ${bga})`;
+        ctx.fillRect(0, 0, drawingBoard.width, drawingBoard.height);
     }
 }
 
 var held = false;
 function isheld(event) {
-    console.log(true);
     held = true;
 }
 function notheld(event) {
-    console.log(false);
     held = false;
 }
 
@@ -36,10 +36,9 @@ var mouse = {
     xBefore: undefined,
     yBefore: undefined,
     drawSize: 5,
-    color: "black",
+    color: `rgba(${r}, ${g}, ${b}, ${a})`,
     draw: function () {
         if (held) {
-            console.log("drawing");
             ctx.beginPath();
             ctx.strokeStyle = this.color;
             ctx.lineCap = "round";
@@ -51,6 +50,92 @@ var mouse = {
         }
     },
     
+}
+
+var droppedColor = false;
+function dropColor() {
+    if (!droppedColor) {
+        droppedColor = true;
+        document.getElementById("colorDropDown").style = "display: flex";
+        document.getElementById("colorDropDown").style.top = `${document.getElementById("colorButton").offsetTop + document.getElementById("colorButton").offsetHeight}px`;
+        document.getElementById("colorDropDown").style.left = `${document.getElementById("colorButton").offsetLeft}px`;
+        document.getElementById("colorDropDown").style.width = `${document.getElementById("colorButton").offsetWidth}px`;
+    } else {
+        droppedColor = false;
+        document.getElementById("colorDropDown").style = "display: none";
+    }
+}
+
+var droppedBackground = false;
+function dropBackground() {
+    if (!droppedBackground) {
+        droppedBackground = true;
+        document.getElementById("backgroundDropDown").style = "display: flex";
+        document.getElementById("backgroundDropDown").style.top = `${document.getElementById("backgroundButton").offsetTop + document.getElementById("backgroundButton").offsetHeight}px`;
+        document.getElementById("backgroundDropDown").style.left = `${document.getElementById("backgroundButton").offsetLeft}px`;
+        document.getElementById("backgroundDropDown").style.background = `${document.getElementById("backgroundButton").offsetbackground}px`;
+    } else {
+        droppedBackground = false;
+        document.getElementById("backgroundDropDown").style = "display: none";
+    }
+}
+
+var droppedWidth = false;
+function dropWidth() {
+    if (!droppedWidth) {
+        droppedWidth = true;
+        document.getElementById("widthDropDown").style = "display: flex";
+        document.getElementById("widthDropDown").style.top = `${document.getElementById("widthButton").offsetTop + document.getElementById("widthButton").offsetHeight}px`;
+        document.getElementById("widthDropDown").style.left = `${document.getElementById("widthButton").offsetLeft}px`;
+        document.getElementById("widthDropDown").style.width = `${document.getElementById("widthButton").offsetWidth}px`;
+    } else {
+        droppedWidth = false;
+        document.getElementById("widthDropDown").style = "display: none";
+    }
+}
+
+var r, g, b, a;
+function changeColor() {
+    r = document.getElementById("red").value;
+    g = document.getElementById("green").value;
+    b = document.getElementById("blue").value;
+    a = document.getElementById("alpha").value;
+    mouse.color = `rgba(${r}, ${g}, ${b}, ${a})`;
+    document.getElementById("color").style.backgroundColor = `rgba(${r}, ${g}, ${b}, ${a})`;
+}
+
+var bgr, bgg, bgb, bga;
+function changeBGColor() {
+    bgr = document.getElementById("BGred").value;
+    bgg = document.getElementById("BGgreen").value;
+    bgb = document.getElementById("BGblue").value;
+    bga = document.getElementById("BGalpha").value;
+    ctx.fillStyle = `rgba(${bgr}, ${bgg}, ${bgb}, ${bga})`;
+    ctx.fillRect(0, 0, drawingBoard.width, drawingBoard.height);
+    document.getElementById("BGcolor").style.backgroundColor = `rgba(${bgr}, ${bgg}, ${bgb}, ${bga})`;
+}
+
+function downloadImage() {
+
+    // Grab the canvas element
+    let canvas = document.getElementById("canvas");
+
+    /* Create a PNG image of the pixels drawn on the canvas using the toDataURL method. PNG is the preferred format since it is supported by all browsers
+    */
+    var dataURL = canvas.toDataURL("image/png");
+
+    // Create a dummy link text
+    var a = document.createElement('a');
+    // Set the link to the image so that when clicked, the image begins downloading
+    a.href = dataURL
+    // Specify the image filename
+    a.download = 'canvas-download.jpeg';
+    // Click on the link to set off download
+    a.click();
+}
+
+function changeWidth() {
+    mouse.drawSize = document.getElementById("width").value;
 }
 
 function init() {
