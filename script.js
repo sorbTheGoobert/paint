d7e365fab1e7888bce21627da445f89702f88a26
= <=>
2024-07-22 12:20:57 +0800

    heh

--- script.js
+++ script.js
@@ -1,26 +1,66 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-canvas.addEventListener("mousedown", );
-
 var drawingBoard = {
     width: 1440,
     height: 900,
-    color: black,
-    load: function() {
+    load: function () {
         canvas.width = `${this.width}`;
         canvas.height = `${this.height}`;
     },
+    clear: function() {
+        ctx.clearRect(0, 0, this.width, this.height);
+    }
 }
 
-function draw() {
-    ctx.fillStyle = drawingBoard.color;
-    ctx.fillRect(Math.floor(event.clientX), Math.floor(event.clientY), 1, 1);
-    console.log("yeah");
+var held = false;
+function isheld(event) {
+    console.log(true);
+    held = true;
+}
+function notheld(event) {
+    console.log(false);
+    held = false;
+}
+
+function checkMousePos(event) {
+    mouse.xBefore = mouse.x;
+    mouse.yBefore = mouse.y;
+    mouse.x = event.offsetX;
+    mouse.y = event.offsetY;
+}
+
+var mouse = {
+    x: undefined,
+    y: undefined,
+    xBefore: undefined,
+    yBefore: undefined,
+    drawSize: 5,
+    color: "black",
+    draw: function () {
+        if (held) {
+            console.log("drawing");
+            ctx.beginPath();
+            ctx.strokeStyle = this.color;
+            ctx.lineCap = "round";
+            ctx.lineWidth = this.drawSize;
+            ctx.moveTo(this.xBefore, this.yBefore);
+            ctx.lineTo(this.x, this.y);
+            ctx.stroke();
+            ctx.closePath();
+        }
+    },
+    
 }
 
 function init() {
     drawingBoard.load();
+    update();
+}
+function update() {
+    // checkMousePos();
+    mouse.draw();
+    requestAnimationFrame(update);
 }
 
 init();
